36706bb0fe2d549d8451a58285a09b5e0104c4a2
Connor Harrison <connorh.uottawa@gmail.com>
2025-12-24 01:49:06 -0500

    feat: add discord component helpers

---
+++ src/components/buildPickBanButtons.ts
@@ -0,0 +1,60 @@
+import { PICK_BAN_CONFIGS } from "@/constants";
+import type { PickBanState } from "@/models";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
+import { StepAction } from "@/models";
+
+export const buildPickBanButtons = (pickBanState: PickBanState) => {
+  const rows: ActionRowBuilder<ButtonBuilder>[] = [];
+  let row = new ActionRowBuilder<ButtonBuilder>();
+
+  const { availableMaps, configKey, currentStepIndex, pickedMaps, channelId } = pickBanState;
+  const stepAction = PICK_BAN_CONFIGS[configKey].steps[currentStepIndex]?.action;
+
+  if (!stepAction) {
+    throw new Error("Invalid current step index");
+  }
+
+  let buttonStyle: ButtonStyle;
+
+  if (stepAction === StepAction.SIDE_PICK) {
+    buttonStyle = ButtonStyle.Secondary;
+    const pickedMap = pickedMaps[pickedMaps.length - 1];
+
+    if (!pickedMap) {
+      throw new Error("No map picked yet for side pick");
+    }
+
+    for (const side of pickedMap.sideOptions) {
+      row.addComponents(
+        new ButtonBuilder().setLabel(side).setStyle(buttonStyle).setCustomId(`${channelId}-${pickedMap.name}-${side}`),
+      );
+    }
+
+    rows.push(row);
+    return rows;
+  }
+
+  if (stepAction === StepAction.MAP_PICK || stepAction === StepAction.BAN) {
+    buttonStyle = stepAction === StepAction.MAP_PICK ? ButtonStyle.Success : ButtonStyle.Danger;
+
+    for (const map of availableMaps) {
+      if (row.components.length === 5) {
+        rows.push(row);
+        row = new ActionRowBuilder<ButtonBuilder>();
+      }
+      const mapName = map.name;
+
+      row.addComponents(
+        new ButtonBuilder()
+          .setLabel(mapName)
+          .setStyle(buttonStyle)
+          .setCustomId(`${channelId}-${mapName}-${stepAction}`),
+      );
+    }
+
+    rows.push(row);
+    return rows;
+  }
+
+  throw new Error("Unsupported step action for button building");
+};
---
+++ src/components/buildPickBanEmbed.ts
@@ -0,0 +1,12 @@
+import type { PickBanState } from "@/models";
+
+export const buildPickBanEmbed = (pickBanState: PickBanState) => {
+  const {log, channelId } = pickBanState;
+
+  const description = log.join("\n");
+  const embed = {
+    title: `Pick/Ban for Match ${channelId}`,
+    description,
+  };
+  return [embed];
+};
